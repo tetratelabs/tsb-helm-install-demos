bebc80b96fe25e2b7debf61d5d59198d05e7a4cd
Alex Skvo <alexskvo@Alexs-MacBook-Pro.local>
2022-08-17 16:06:13 -0400

    added bonus bookinfo internal igw

---
+++ bonus_bookinfo_internal_igw/step_2_apply_tsb_objects.sh
@@ -0,0 +1,84 @@
+cat > "${FOLDER}/tsb_objects.yaml" <<EOF
+---
+apiVersion: api.tsb.tetrate.io/v2
+kind: Tenant
+metadata:
+  organization: tetrate
+  name: tetrate
+spec:
+  displayName: Tetrate
+---
+apiversion: api.tsb.tetrate.io/v2
+kind: Workspace
+metadata:
+  organization: tetrate
+  tenant: tetrate
+  name: bookinfo-ws
+spec:
+  namespaceSelector:
+    names:
+      - "*/bookinfo"
+---
+apiVersion: gateway.tsb.tetrate.io/v2
+kind: Group
+metadata:
+  organization: tetrate
+  tenant: tetrate
+  workspace: bookinfo-ws
+  name: bookinfo-gw
+spec:
+  namespaceSelector:
+    names:
+      - "*/bookinfo"
+  configMode: BRIDGED
+---
+apiVersion: traffic.tsb.tetrate.io/v2
+kind: Group
+Metadata:
+  organization: tetrate
+  tenant: tetrate
+  workspace: bookinfo-ws
+  name: bookinfo-traffic
+spec:
+  namespaceSelector:
+    names:
+      - "*/bookinfo"
+  configMode: BRIDGED
+---
+apiVersion: security.tsb.tetrate.io/v2
+kind: Group
+Metadata:
+  organization: tetrate
+  tenant: tetrate
+  workspace: bookinfo-ws
+  name: bookinfo-security
+spec:
+  namespaceSelector:
+    names:
+      - "*/bookinfo"
+  configMode: BRIDGED
+---
+apiVersion: gateway.tsb.tetrate.io/v2
+kind: IngressGateway
+Metadata:
+  organization: tetrate
+  name: bookinfo-gw-ingress
+  group: bookinfo-gw
+  workspace: bookinfo-ws
+  tenant: tetrate
+spec:
+  workloadSelector:
+    namespace: bookinfo
+    labels:
+      app: tsb-gateway-bookinfo
+  http:
+    - name: bookinfo
+      port: 80
+      hostname: "bookinfo.tetrate.com"
+      routing:
+        rules:
+          - route:
+              host: "bookinfo/productpage.bookinfo.svc.cluster.local"
+EOF
+
+tctl apply -f "${FOLDER}/tsb_objects.yaml" 
